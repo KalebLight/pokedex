cff96afb2cb5ea4459116ea18567f08a745c4c06
KalebLight <kaleb.luiz00@gmail.com>
2022-05-10 23:35:03 -0300

    Subindo componente Home

---
+++ project-pokedex/src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import Button from '@mui/material/Button';
+import { Fragment } from 'react';
+import Navbar from '../Navbar/Navbar';
+import useStyles from './styles';
+
+function Home() {
+  const classes = useStyles();
+  return (
+    <div className={classes.jhsdhjgasdhjgashdjg}>
+      <Navbar />
+      {/* <Button className={classes.btn}>dsds</Button> */}
+    </div>
+  );
+}
+
+export default Home;
---
+++ project-pokedex/src/Pages/Home/styles.js
@@ -0,0 +1,17 @@
+import { makeStyles } from '@material-ui/styles';
+import { COLORS } from '../../Styles/colors';
+
+export default makeStyles((theme) => ({
+  jhsdhjgasdhjgashdjg: {
+    padding: '10px !important',
+  },
+  app: {
+    backgroundColor: COLORS.black,
+    height: '100% !important',
+  },
+  btn: {
+    fontSize: 20,
+    backgroundColor: COLORS.themeSecondary,
+    height: '40px',
+  },
+}));
